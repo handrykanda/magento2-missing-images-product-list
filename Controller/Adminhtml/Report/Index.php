72307efca8fb795961a7130458bcac6f22d92869
handrkanda <handrykanda@gmail.com>
2020-09-22 14:54:06 +0200

    Initial commit

---
+++ Controller/Adminhtml/Report/Index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @category   ThreeDegrees
+ * @package    ThreeDegrees_MissingProductImagesReport
+ * @author     ThreeDegrees Handry
+ */
+namespace ThreeDegrees\MissingProductImagesReport\Controller\Adminhtml\Report;
+
+class Index extends \Magento\Backend\App\Action
+  {
+    /**
+    * @var \Magento\Framework\View\Result\PageFactory
+    */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+         parent::__construct($context);
+         $this->resultPageFactory = $resultPageFactory;
+    }
+
+
+
+    protected function _isAllowed()
+    {
+     return $this->_authorization->isAllowed('ThreeDegrees_MissingProductImagesReport::report');
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/exampleadminnewpage_helloworld_index.xml
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+         return  $resultPage = $this->resultPageFactory->create();
+    }
+  }
+?>
---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @category   ThreeDegrees
+ * @package    ThreeDegrees_MissingProductImagesReport
+ * @author     ThreeDegrees Handry
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ThreeDegrees_MissingProductImagesReport',
+    __DIR__
+);
